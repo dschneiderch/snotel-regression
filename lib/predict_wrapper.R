d5fe1c5494580d5c19326b1294afe840f31c0dd2
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 10:16:02 -0700

    fixed netcdf output using ncdf4 package. fixed bug in dim3 and added projection string to global attribute of files.

--- lib/predict_wrapper.R
+++ lib/predict_wrapper.R
@@ -10,22 +10,20 @@ predict_wrapper <- function(rdtsdF,style,newdata,newdatalocs,spatialblend){
      #output rasters of each model type.
      dim1=ncdim_def('Long','degree',seq(-112.25,-104.125,0.00416666667))
      dim2=ncdim_def('Lat','degree',seq(33,43.75,0.00416666667))
-     dim3=ncdim_def('time','yrdoy',unlim=T,vals=fullpreds$yrdoy)
+     dim3=ncdim_def('time','yrdoy',unlim=T,vals=unique(fullpreds$yrdoy))
      var=ncvar_def('swe','meters',dim=list(dim1,dim2,dim3),missval=-99,longname='snow water equivalent',compression=6)
      yr=unique(rdtsdF$yr)
      phv.fn=file.path(bpth,'netcdf',paste0('fullpreds-phv_',yr,'_',spatialblend,'.nc'))
      phvrcn.fn=file.path(bpth,'netcdf',paste0('fullpreds-phvrcn_',yr,'_',spatialblend,'.nc'))
      phv.nc=nc_create(phv.fn,var)
      phvrcn.nc=nc_create(phvrcn.fn,var)
-     tind=fullpreds$yrdoy
+     tind=unique(fullpreds$yrdoy)
      
-     GIdf=ddply(fullpreds,.(yrdoy),function(dF){
-          
-          ncvar_put(phvnc, var, vals=dF$phv.fullpred,start=c(1,1,which(tind==unique(dF$yrdoy))),count=c(-1,-1,1))
-          ncvar_put(phvrcnnc,var,vals=dF$phvrcn.fullpred,start=c(1,1,which(tind==unique(dF$yrdoy))),count=c(-1,-1,1))
-          ncatt_put(ncnew,0,'proj4string','+proj=longlat +datum=NAD83')
-          
-          plot_localmoran(dF,snotellocs.usgs,recon.version)     
+     d_ply(fullpreds,.(yrdoy),function(dF){
+          ncvar_put(phv.nc, var, vals=dF$phv.fullpred,start=c(1,1,which(unique(dF$yrdoy)==tind)),count=c(-1,-1,1))
+          ncvar_put(phvrcn.nc,var,vals=dF$phvrcn.fullpred,start=c(1,1,which(tind==unique(dF$yrdoy))),count=c(-1,-1,1))
+          ncatt_put(phv.nc,0,'proj4string','+proj=longlat +datum=NAD83')
+          ncatt_put(phvrcn.nc,0,'proj4string','+proj=longlat +datum=NAD83')
      })
      
      nc_close(phvnc)

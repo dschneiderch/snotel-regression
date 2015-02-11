92d67e21aa5ac3cf845b7f4631365829b50187f4
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 09:56:54 -0700

    attempted to limit the scope of options(warn=2) to the phvrcnfit function by wrapping in another function. attempt is commented out but available for future

--- lib/CVwrapper.R
+++ lib/CVwrapper.R
@@ -2,6 +2,7 @@
 # produce cross val predictions for all dyn iterations for a given doy.
 CVwrapper <- function(xrdate,doydF,static_mdl){
      print(paste0('--recondate: ',xrdate))
+#      phvrcn.out=withOptions(list(warn=2),doPHVRCNfit(xrdate,doydF,static_mdl))
      phvrcn.out=doPHVRCNfit(xrdate,doydF,static_mdl)
      dyn_mdl=phvrcn.out[[1]]
      rcn.sig.phv=phvrcn.out[[2]]

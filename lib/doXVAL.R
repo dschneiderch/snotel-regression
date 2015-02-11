4b50b54f36cda72ac0e845e0b47fd2bf092096e3
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 10:10:18 -0700

    no changes....just moved some code around?

--- lib/doXVAL.R
+++ lib/doXVAL.R
@@ -1,34 +1,35 @@
 #does cross validation for a doy and an iteration
 doXVAL <- function(doy_rdoydF,static_mdl,dyn_mdl,rcn.sig.phv){
      #    
-if(!is.na(static_mdl) && !is.na(dyn_mdl)){
-     ## cross validation
-     myglm=function(formula,data) {
-          glm(formula,data,family=gaussian(link='log'))
+     if(!is.na(static_mdl) && !is.na(dyn_mdl)){
+          ## cross validation
+          myglm= function(formula,data) {
+               glm(formula,data,family=gaussian(link='log'))
+          }
+          mypredict.glm <- function(object, newdata){
+               predict(object, newdata,type='response')
+          }
+          #PHV only surface
+          phv=errorest(formula=formula(static_mdl),data=doy_rdoydF, model=myglm, predict=mypredict.glm, estimator='cv',est.para=control.errorest(k=nrow(doy_rdoydF), predictions=T))
+          #PHV and RECON surface
+          phvrcn=errorest(formula=formula(dyn_mdl),data=doy_rdoydF, model=myglm, predict=mypredict.glm, estimator='cv',est.para=control.errorest(k=nrow(doy_rdoydF), predictions=T))
+
+          
+     } else {
+          phv=data.frame(predictions=rep(NA,nrow(doy_rdoydF)))
+          phvrcn=data.frame(predictions=rep(NA,nrow(doy_rdoydF)))
      }
-     mypredict.glm <- function(object, newdata){
-          predict(object, newdata,type='response')
-     }
-     #PHV only surface
-     phv=errorest(formula=formula(static_mdl),data=doy_rdoydF, model=myglm, predict=mypredict.glm, estimator='cv',est.para=control.errorest(k=nrow(doy_rdoydF), predictions=T))
-     #PHV and RECON surface
-     phvrcn=errorest(formula=formula(dyn_mdl),data=doy_rdoydF, model=myglm, predict=mypredict.glm, estimator='cv',est.para=control.errorest(k=nrow(doy_rdoydF), predictions=T))
-     
-} else {
-     phv=data.frame(predictions=rep(NA,nrow(doy_rdoydF)))
-     phvrcn=data.frame(predictions=rep(NA,nrow(doy_rdoydF)))
-}
-#
-#print(str(doy_rdoydF))
-return(     data.frame(
-                    date=doy_rdoydF$date,
-                    yrdoy=doy_rdoydF$yrdoy,
-                    recondate=doy_rdoydF$recondate,
-                    Station_ID=doy_rdoydF$Station_ID,
-                    snotel=doy_rdoydF$snotel,
-                    phv.predictions=phv$predictions,
-                    phvrcn.predictions=phvrcn$predictions,
-                    recon=unscale_vec(doy_rdoydF$recon),
-                    sig=rcn.sig.phv)
+     #
+     #print(str(doy_rdoydF))
+     return(     data.frame(
+          date=doy_rdoydF$date,
+          yrdoy=doy_rdoydF$yrdoy,
+          recondate=doy_rdoydF$recondate,
+          Station_ID=doy_rdoydF$Station_ID,
+          snotel=doy_rdoydF$snotel,
+          phv.predictions=phv$predictions,
+          phvrcn.predictions=phvrcn$predictions,
+          recon=unscale_vec(doy_rdoydF$recon),
+          sig=rcn.sig.phv)
      )
 }

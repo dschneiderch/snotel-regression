be9db404673e16ae68b70cc91d7866b98125c562
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 10:06:16 -0700

    added recon resid calculation and kriging prediction. commented code to produce daily .grd files.

--- lib/doSPATIAL.R
+++ lib/doSPATIAL.R
@@ -8,33 +8,36 @@ doSPATIAL=function(dF, snotellocs.usgs, app, newdata=NULL,bpth,spatialblend){
      #print(nrow(locsmat))
      #print(str(dF))
      #kr=Krig(locsmat,dF$phvresid,theta=200000)
+     rresidkr=Krig(locsmat,dF$reconresid,theta=200000)
      sresidkr=Krig(locsmat,dF$phvresid,theta=200000)
      dresidkr=Krig(locsmat,dF$phvrcnresid,theta=200000)
      #residtps=Tps(locsmat,dF$phvresid)
      #print(residkr)
      if(app=='xval'){
           phv.fullpred=dF$phv.predictions-predict(sresidkr,x=locsmat)
-          phvrcn.fullpred=dF$phvrcn.predictions-predict(sresidkr,x=locsmat,y=dF$phvrcnresid)
-          return(
-               data.frame(
-                    date=dF$date,
-                    yrdoy=dF$yrdoy,
-                    snotel=dF$snotel,
-                    recondate=dF$recondate,
-                    recon=dF$recon,
-                    phvresid=dF$phvresid,
-                    phvrcnresid=dF$phvrcnresid,
-                    reconresid=dF$reconresid,
-                    phv.pred=dF$phv.predictions,
-                    phvrcn.pred=dF$phvrcn.predictions, 
-                    phv.fullpred,
-                    phvrcn.fullpred)
-          )
+          phvrcn.fullpred=dF$phvrcn.predictions-predict(dresidkr,x=locsmat)
+          recon.fullpred=dF$recon-predict(rresidkr,x=locsmat)
+          
+          snotelpreds= data.frame(
+                         date=dF$date,
+                         yrdoy=dF$yrdoy,
+                         snotel=dF$snotel,
+                         recondate=dF$recondate,
+                         recon=dF$recon,
+                         phvresid=dF$phvresid,
+                         phvrcnresid=dF$phvrcnresid,
+                         reconresid=dF$reconresid,
+                         phv.pred=dF$phv.predictions,
+                         phvrcn.pred=dF$phvrcn.predictions, 
+                         phv.fullpred,
+                         phvrcn.fullpred,
+                         recon.fullpred)
+          return(snotelpreds)
      }
      if(app=='predict'){
           #bpn=file.path(bpn,'raster')
-          pfn=paste0('fullpred_phv-',unique(dF$date),'-',spatialblend,'.grd')
-          prfn=paste0('fullpred_phvrcn-',unique(dF$date),'-',spatialblend,'.grd')
+#           pfn=paste0('fullpred_phv-',unique(dF$date),'-',spatialblend,'.grd')
+#           prfn=paste0('fullpred_phvrcn-',unique(dF$date),'-',spatialblend,'.grd')
           #
           phv.fullpred=newdata$phv.predictions-predict(sresidkr,x=newdata[,c('x','y')])
           phvrcn.fullpred=newdata$phvrcn.predictions-predict(dresidkr,x=newdata[,c('x','y')])        

1f35a779896fcb8d8ebcd8c70743df970c68c5dc
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 14:08:39 -0700

    fixed netcdf output to write right side up. changed output of ddply from fullpreds to newdatapredsfull

--- lib/predict_wrapper.R
+++ lib/predict_wrapper.R
@@ -5,30 +5,34 @@ predict_wrapper <- function(rdtsdF,style,newdata,newdatalocs,spatialblend){
      print(paste('--',unique(rdtsdF$yr),'--'))
      bpth=paste0('diagnostics/rswe_',recon.version,'/fullpreds')
      
-     fullpreds=ddply(rdtsdF,.(yrdoy),predict_surfaces,snotellocs.usgs,newdata,newdatalocs,spatialblend,bpth,.inform=F,.parallel=F)#,.paropts=list(.export=ls(), .packages=.packages(all=T)))     
+     newdatapredsfull=ddply(rdtsdF,.(yrdoy),predict_surfaces,snotellocs.usgs,newdata,newdatalocs,spatialblend,bpth,.inform=F,.parallel=F)#,.paropts=list(.export=ls(), .packages=.packages(all=T)))     
      
      #output rasters of each model type.
      dim1=ncdim_def('Long','degree',seq(-112.25,-104.125,0.00416666667))
      dim2=ncdim_def('Lat','degree',seq(33,43.75,0.00416666667))
-     dim3=ncdim_def('time','yrdoy',unlim=T,vals=unique(fullpreds$yrdoy))
+     dim3=ncdim_def('time','yrdoy',unlim=T,vals=as.numeric(unique(newdatapredsfull$yrdoy)))
      var=ncvar_def('swe','meters',dim=list(dim1,dim2,dim3),missval=-99,longname='snow water equivalent',compression=6)
      yr=unique(rdtsdF$yr)
      phv.fn=file.path(bpth,'netcdf',paste0('fullpreds-phv_',yr,'_',spatialblend,'.nc'))
      phvrcn.fn=file.path(bpth,'netcdf',paste0('fullpreds-phvrcn_',yr,'_',spatialblend,'.nc'))
      phv.nc=nc_create(phv.fn,var)
      phvrcn.nc=nc_create(phvrcn.fn,var)
-     tind=unique(fullpreds$yrdoy)
+     tind=unique(newdatapredsfull$yrdoy)
      
-     d_ply(fullpreds,.(yrdoy),function(dF){
-          ncvar_put(phv.nc, var, vals=dF$phv.fullpred,start=c(1,1,which(unique(dF$yrdoy)==tind)),count=c(-1,-1,1))
-          ncvar_put(phvrcn.nc,var,vals=dF$phvrcn.fullpred,start=c(1,1,which(tind==unique(dF$yrdoy))),count=c(-1,-1,1))
+     d_ply(newdatapredsfull,.(yrdoy),function(dF){
+          #do some data gymnastics to get it in the right order
+          val=matrix(dF$phv.fullpred,nrow=2580,byrow=T)
+          val=t(val[nrow(val):1,])
+          ncvar_put(phv.nc, var, vals=val,start=c(1,1,which(unique(dF$yrdoy)==tind)),count=c(-1,-1,1))
+          #
+          val=matrix(dF$phvrcn.fullpred,nrow=2580,byrow=T)
+          val=t(val[nrow(val):1,])          
+          ncvar_put(phvrcn.nc,var,vals=val,start=c(1,1,which(tind==unique(dF$yrdoy))),count=c(-1,-1,1))
+          #
           ncatt_put(phv.nc,0,'proj4string','+proj=longlat +datum=NAD83')
           ncatt_put(phvrcn.nc,0,'proj4string','+proj=longlat +datum=NAD83')
      })
+     nc_close(phv.nc)
+     nc_close(phvrcn.nc)
      
-     nc_close(phvnc)
-     nc_close(phvrcnnc)
-     
-     #     
-     #      
 }

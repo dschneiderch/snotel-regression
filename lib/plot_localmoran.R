26956afa91e98948ef521360fce0bf4853259a2d
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 10:12:15 -0700

    changed locs variable to snotellocs.usgs to be consistent with other code. annotated plots as being unblended. added blended I and pvalue to output dataframe

--- lib/plot_localmoran.R
+++ lib/plot_localmoran.R
@@ -1,8 +1,11 @@
+## TODO plot both blended and unblended estimates, create 1 dataframe to facet.
+
 plot_localmoran=function(dF,snotellocs.usgs,recon.version){
+     snotellocs.df=as.data.frame(snotellocs.usgs)
      maxdist=200000
-     kd=dnearneigh(locs,d1=0,d2=maxdist,row.names=snotellocs.usgs$Station_ID)#d2 should equal abut 200km fro all stations to have some neighbors. min 8 with 200km.
-summary(kd)
-     plot(kd,coords=coordinates(snotellocs.usgs))#
+     kd=dnearneigh(snotellocs.usgs,d1=0,d2=maxdist,row.names=snotellocs.usgs$Station_ID)#d2 should equal abut 200km fro all stations to have some neighbors. min 8 with 200km.
+     #summary(kd)
+    # plot(kd,coords=coordinates(snotellocs.usgs))#
      getneighbors=function(nm){
           ind=grep(nm,attr(kd,'region.id'))
           vec=kd[[ind]]
@@ -18,7 +21,7 @@ summary(kd)
      locsmat=as.matrix(as.data.frame(snotellocs.usgs)[,c('x','y')])    
      exdat=data.frame(locsmat,dF)
      localmoran4plot=function(localm){
-          dF=cbind(as.data.frame(localm),x=locs.df$x,y=locs.df$y)
+          dF=cbind(as.data.frame(localm),x=snotellocs.df$x,y=snotellocs.df$y)
           dF=mutate(dF,
                     Pcut=cut(dF$P,c(0,0.01,0.05,0.1,max(dF$P))),
                     shapecut=as.factor(sign(Ii)))
@@ -40,7 +43,7 @@ mt1=moran.test(exdat$phv.pred,listw=kd.idw)
           geom_path(data=states.usgs,aes(x,y,group=group))+
           annotate('text',x=-9e5,y=1.25e6,
                    label=paste0('Global I = ', format(mt1$estimate[1],digits=3),'\n','p-value = ',format(mt1$p.value,digits=3)))+
-          labs(title='phv regression')+
+          labs(title='phv regression (no blend)')+
           coord_cartesian(xlim=extent(usgsbox)[1:2],ylim=extent(usgsbox)[3:4])+
           theme_minimal()
 
@@ -50,7 +53,7 @@ mt2=moran.test(exdat$phvrcn.pred,listw=kd.idw)
      geom_path(data=states.usgs,aes(x,y,group=group))+
           annotate('text',x=-9e5,y=1.25e6,
               label=paste0('Global I = ', format(mt2$estimate[1],digits=3),'\n','p-value = ',format(mt2$p.value,digits=3)))+
-          labs(title='phvrcn regression')+
+          labs(title='phvrcn regression (no blend)')+
      coord_cartesian(xlim=extent(usgsbox)[1:2],ylim=extent(usgsbox)[3:4])+
           theme_minimal()
 
@@ -58,15 +61,24 @@ ga=arrangeGrob(p,pr)
 
 xrdate=unique(dF$recondate)
 dt=unique(dF$date)
-ggsave(ga,width=6,height=10,filename=paste0('diagnostics/rswe_',recon.version,'/localmoran/localmoran_',dt,'_recondate-',xrdate,'_recon_',recon.version,'.pdf'))
+ggsave(ga,width=6,height=10,filename=paste0('diagnostics/rswe_',recon.version,'/localmoran/xval_localmoran_',dt,'_recondate-',xrdate,'_recon_',recon.version,'.pdf'))
  #    moran.plot(exdat$phvrcn.pred,kd.idw)
 
-return(data.frame(
+mt3=moran.test(exdat$phv.fullpred,listw=kd.idw)
+mt4=moran.test(exdat$phvrcn.fullpred,listw=kd.idw)
+
+return(
+     data.frame(
      date=dt,
      recondate=xrdate,
-     phv.full.globalI=mt1$estimate[1],
-     phvrcn.full.globalI=mt2$estimate[1],
-     phv.full.pvalue=mt1$pvalue,
-     phvrcn.full.pvalue=mt2$pvalue))
+     phv.globalI=mt1$estimate[1],
+     phvrcn.globalI=mt2$estimate[1],
+     phv.pvalue=mt1$p.value,
+     phvrcn.pvalue=mt2$p.value,
+     phv.full.globalI=mt3$estimate[1],
+     phvrcn.full.globalI=mt4$estimate[1],
+     phv.full.pvalue=mt3$p.value,
+     phvrcn.full.pvalue=mt4$p.value)
+     )
 
 }

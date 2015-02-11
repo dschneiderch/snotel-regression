c8ecf34c3c0498da6cf6a82535e1480c89d364fa
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-11 16:53:41 -0700

    put if statements to top, generalized max and which.max based on cost function

--- lib/doCOST.R
+++ lib/doCOST.R
@@ -3,47 +3,44 @@ doCOST=function(mdlpreds,cost,recon.version){
      
      if(cost=='cor') { #statistics to maximize
           costfun<-function(y,yhat) cor(y,yhat)
-     } else if (cost=='r2'){     
+          opti=max
+          which.opti=which.max
+     } else if (cost=='r2') {     
           costfun<-function(y,yhat) 1-sum((y-yhat)^2)/sum((y-mean(y,na.rm=T))^2)
+          opti=max
+          which.opti=which.max
+     } else if(cost=='mae') { #statistics to minimize
+          costfun<-function(y, yhat) mean(abs(yhat-y),na.rm=T)
+          opti=min 
+          which.opti=which.min
+     } else if(cost=='rmse') {
+          costfun<-function(y,yhat) sqrt(mean((yhat-y)^2),na.rm=T)
+          opti=min
+          which.opti=which.min
      }
-     if(exists('costfun')){
-          daystats=ddply(mdlpreds,.(yrdoy,recondate), 
-                         function(x){
-                              summarise(x,  
-                                        phvcor=costfun(snotel,phv.fullpred),
-                                        phvrcncor=costfun(snotel,phvrcn.fullpred),
-                                        reconcor=costfun(snotel,recon))
-                                        }
-                         )
-          daystats$date=as.POSIXct(strptime(daystats$yrdoy,'%Y%j'))
-          whichrdate=ddply(daystats,.(date),summarise,
-                           yr=strftime(unique(date),'%Y'),
-                           phvrcn_recondate=recondate[which.max(phvrcncor)],
-                           recon_costdate=recondate[which.max(reconcor)],
-                           skill_phv=max(phvcor),
-                           skill_phvrcn=max(phvrcncor),
-                           skill_recon=max(reconcor))
-     } else { #statistics to minimize
-          if(cost=='mae') costfun<-function(y, yhat) mean(abs(yhat-y),na.rm=T)
-          if(cost=='rmse') costfun<-function(y,yhat) sqrt(mean((yhat-y)^2),na.rm=T)
      
-          daystats=ddply(mdlpreds,.(yrdoy,recondate),function(x){
-                         summarise(x, 
-                              phvcor=costfun(snotel,phv.fullpred),
-                              phvrcncor=costfun(snotel,phvrcn.fullpred),
-                              reconcor=costfun(snotel,recon))
-                         }
-                         )
-          daystats$date=as.POSIXct(strptime(daystats$yrdoy,'%Y%j'))
-          whichrdate=ddply(daystats,.(date),summarise,
-                           yr=strftime(unique(date),'%Y'),
-                           phvrcn_recondate=recondate[which.min(phvrcncor)],
-                           recon_costdate=recondate[which.min(reconcor)],
-                           skill_phv=min(phvcor),
-                           skill_phvrcn=min(phvrcncor),
-                           skill_recon=min(reconcor))
-     }     
+     daystats=ddply(mdlpreds,.(yrdoy,recondate),function(x){
+          summarise(x, 
+                    phv.cor=costfun(snotel,phv.pred),
+                    phvrcn.cor=costfun(snotel,phvrcn.pred),
+                    phvfull.cor=costfun(snotel,phv.fullpred),
+                    phvrcnfull.cor=costfun(snotel,phvrcn.fullpred),
+                    recon.cor=costfun(snotel,recon),
+                    reconfull.cor=costfun(snotel,recon.fullpred))
+     }
+     )
+     daystats$date=as.POSIXct(strptime(daystats$yrdoy,'%Y%j'))
+     
+     whichrdate=ddply(daystats,.(date),summarise,
+                      yr=strftime(unique(date),'%Y'),
+                      phvrcn_recondate=recondate[which.opti(phvrcncor)],
+                      recon_costdate=recondate[which.opti(reconcor)],
+                      skill_phv=opti(phv.cor),
+                      skill_phvrcn=opti(phvrcn.cor),
+                      skill_recon=opti(recon.cor),
+                      skill_phvfull=opti(phvfull.cor),
+                      skill_phvrcnful=opti(phvrcnfull.cor),
+                      skill_reconfull=opti(reconfull.cor))
      
      return(whichrdate)
 }
-

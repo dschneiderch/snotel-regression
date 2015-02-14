03eb167566f9f44ffd233460df4598e4bb966d3a
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-13 18:16:34 -0700

    fixed logic for finding NAs. converting summer NA to 0

--- lib/fixNA.R
+++ lib/fixNA.R
@@ -1,8 +1,14 @@
-fixNA = function(dF,colnm){
+fixSWE = function(dF,colnm){
      dF=dF[order(dF$Station_ID,dF$date),]
-     ind=which(is.na(dF[dF$snoteldate/10000<7 | dF$snoteldate/10000>10,colnm]))
+     dF$mth=as.numeric(strftime(dF$date,'%m'))
+     summerNA=which(dF$mth>=7 & dF$mth<11 & is.na(dF[,colnm]))
+     dF[summerNA,colnm]=0
+     #print(dF[summerNA,c('date','Station_ID','snoteldate','swe')])
+     newdF=dF
+     ind=which((dF$mth<7 | dF$mth>=11 ) & is.na(dF[,colnm]))
+     print(dF[ind,c('date','Station_ID','snoteldate','swe')])
      if(length(ind)>0){
-          winsize=10#needs to be >=10
+          winsize=15#needs to be >=10
           dFfix=ldply(as.list(ind),function(i){     
                tmp=dF[(i-winsize):(i+winsize),colnm]
                tmpna=which(is.na(tmp))
@@ -19,13 +25,12 @@ fixNA = function(dF,colnm){
                     x=tmpna-min(tmpna)+1
                     y[is.na(y)]=m*x+iv1
                }
-               data.frame(snotel=y,ind=seq((i-winsize),(i+winsize)))
+               data.frame(colnew=y,ind=seq((i-winsize),(i+winsize)))
                #dF[(i-winsize):(i+winsize),colnm]=y
                #           return(dF)
           })
-          newdF=dF
-          newdF[dFfix$ind,'snotel']=dFfix$snotel
-          return(newdF)
-     } else return(dF)
+          newdF[dFfix$ind,colnm]=dFfix[,'colnew']
+     }
+     return(newdF)
      
 }

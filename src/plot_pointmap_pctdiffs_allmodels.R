784627f704fb9858f7b412db147ee6df54d49ab2
Dominik Schneider <dominik.schneider@colorado.edu>
2015-02-24 18:20:37 -0700

    forgot to save a file before adding...

--- src/plot_pointmap_pctdiffs_allmodels.R
+++ src/plot_pointmap_pctdiffs_allmodels.R
@@ -28,7 +28,7 @@ brkpoints=11
 outlier=250
 swediffmapplot$value=swediffmapplot$value*100
 swediffmapplot$value[swediffmapplot$value > outlier]=outlier-1
-swediffmapplot$value[swediffmapplot$value < -outlier]=-outlier+1
+swediffmapplot$value[swediffmapplot$value < -outlier | is.na(swediffmapplot$value)]=-outlier+1
 swediffmapplot$cuts=cut(swediffmapplot$value,breaks=seq(-outlier,outlier,length.out=brkpoints),right=F,include.lowest=T)
 swediffmapplot$cuts=factor(swediffmapplot$cuts,levels=rev(levels(swediffmapplot$cuts)))
 #
@@ -38,10 +38,11 @@ legendlabels[1]=paste('>= ',outlabel,sep='')
 legendlabels[length(levels(swediffmapplot$cuts))]=paste('< ',-outlabel,sep='')
 #
 szmin=2
-szstep=.5
+szstep=1
 szmax=ceiling((szmin+szstep*length(levels(swediffmapplot$cuts)))/2)
 #
-ggplot()+
+myplot=function(){
+	ggplot()+
     geom_raster(data=ucorelief.df,aes(x=long,y=lat,alpha=ter))+
     scale_alpha_continuous(range=c(1,0.5))+
     geom_path(data=states,aes(x=long,y=lat,group=group),color='grey10')+
@@ -52,15 +53,17 @@ ggplot()+
     scale_size_manual(values=c(seq(szmax,szmin,-szstep),seq(szmin,szmax,szstep)),drop=F,labels=legendlabels)+
     labs(x='Longitude',y='Latitude')+
     guides(alpha=FALSE,
-           size=guide_legend('SWE Differences (%)',reverse=F),
-           colour=guide_legend('SWE Differences (%)',reverse=F),
-           shape=guide_legend('SWE Differences (%)'),reverse=F)+
-    coord_cartesian(xlim=c(-112.25,-104.125),ylim=c(33,43.75))+
+           size=guide_legend('SWE\nDifferences (%)',reverse=F),
+           colour=guide_legend('SWE\nDifferences (%)',reverse=F),
+           shape=guide_legend('SWE\nDifferences (%)'),reverse=F)+
+    # coord_cartesian(xlim=c(-112.25,-104.125),ylim=c(33,43.75))+
+    coord_fixed(ratio=10/10,xlim=c(-112.25,-104.125),ylim=c(33,43.75))+
     theme_bw()+
     theme(legend.key=element_rect(fill='grey80'),
           legend.key.size=unit(1.5,'lines'),
           legend.text=element_text(size=14),
           legend.title=element_text(size=16),
+          # aspect.ratio=4/3,
           ## legend.justification='right',
           legend.background=element_rect(fill='grey80'),
           legend.position='right',
@@ -70,5 +73,7 @@ ggplot()+
           strip.text=element_text(size=14,face='bold'))+
     ggtitle('April 1 % Differences\nwith Observed SNOTEL SWE\n')+
     facet_grid(variable~yr,labeller=plot_labeller)
-
-ggsave(paste0('graphs/rswe_',recon.version,'/allmodels.pctdiff.2002.2011.png',dpi=300,width=11,height=8)
+}
+myplot()
+ggsave(paste0('graphs/rswe_',recon.version,'/allmodels.pctdiff.2000.2011.png'),dpi=300,width=11,height=10)
+# ggsave(paste0('graphs/rswe_',recon.version,'/allmodels.pctdiff.allyears.png'),dpi=300,width=24,height=9.5)
